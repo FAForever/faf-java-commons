17ee76dd1e6c02f162e57d1bcaaffc061a1a66a4
Sheikah45 <Sheikah450@gmail.com>
2021-03-19 16:45:29 -0400

    optimize imports

--- faf-commons-api/src/main/java/com/faforever/commons/api/dto/MapPoolAssignment.java
+++ faf-commons-api/src/main/java/com/faforever/commons/api/dto/MapPoolAssignment.java
@@ -1,15 +1,9 @@
 package com.faforever.commons.api.dto;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonSetter;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.jasminb.jsonapi.annotations.Relationship;
 import com.github.jasminb.jsonapi.annotations.Type;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.io.IOException;
 
 @Getter
 @Setter
