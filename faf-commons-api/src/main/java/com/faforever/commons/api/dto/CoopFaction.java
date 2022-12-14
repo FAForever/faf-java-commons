de337cd50b7ff189639caace0deb75e87ca524e4
Ivan <v23620@gmail.com>
2022-12-14 13:07:48 +0300

    resolve comments

--- faf-commons-api/src/main/java/com/faforever/commons/api/dto/CoopFaction.java
+++ faf-commons-api/src/main/java/com/faforever/commons/api/dto/CoopFaction.java
@@ -1,7 +1,5 @@
 package com.faforever.commons.api.dto;
 
 public enum CoopFaction {
-
-  // Order is crucial
   UEF, CYBRAN, AEON, SERAPHIM, CUSTOM
 }
--- faf-commons-api/src/main/java/com/faforever/commons/api/dto/CoopType.java
+++ faf-commons-api/src/main/java/com/faforever/commons/api/dto/CoopType.java
@@ -1,7 +1,5 @@
 package com.faforever.commons.api.dto;
 
 public enum CoopType {
-
-  // Order is crucial
   SC, SCFA, CUSTOM
 }
