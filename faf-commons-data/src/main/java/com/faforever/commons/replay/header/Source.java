07bee2553683c0662d68ea138f5f962097eca2fb
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-05-11 11:47:23 +0200

    Fix typo in name

--- faf-commons-data/src/main/java/com/faforever/commons/replay/header/Source.java
+++ faf-commons-data/src/main/java/com/faforever/commons/replay/header/Source.java
@@ -3,7 +3,8 @@ package com.faforever.commons.replay.header;
 /**
  * Populated by the engine, it represents the client identifiers and names of players that are connected to the game. The list includes observers.
  * @param name
+ * @param playerId
  * @param sourceId
  */
-public record Source(int sourceId, int PlayerId, String name) {
+public record Source(int sourceId, int playerId, String name) {
 }
